6becac34f69cebbec70bfe4fba4ffc4a5ddd687a
ayoub129 <80260703+ayoub129@users.noreply.github.com>
2023-07-08 19:24:27 +0200

    Update WorkWith.js
    Update WorkWith.js

--- src/components/WorkWith.js
+++ src/components/WorkWith.js
@@ -56,7 +56,7 @@ const WorkWith = () => {
       <h3 className="text-green-700 font-bold tracking-wider mb-10">
         WE WORK WITH
       </h3>
-      <div className="grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-11/12 mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-11/12 mx-auto">
         {data.map((d) => (
           <WorkWithItem
             key={d.id}
