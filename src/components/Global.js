d989b795ab6a46dd7145bee1cea5ef1d1c9d7b53
ayoub129 <80260703+ayoub129@users.noreply.github.com>
2023-07-31 16:32:59 +0100

    Update Global.js

--- src/components/Global.js
+++ src/components/Global.js
@@ -12,31 +12,31 @@ const Global = () => {
   const data = [
     {
       id: 1,
-      Number: "6,600",
-      description: "CUSTOMERS",
+      Number: "100+ SATISFIED CLIENTS",
+      description: "A testament to our commitment to superior service and client satisfaction.",
     },
     {
       id: 2,
-      Number: "$2.7",
+      Number: "10+ ICONIC DEVELOPMENTS IN TANGER",
       description:
-        "TRILLION ECONOMIC VALUE OF GOODS MOVE THROUGH OUR FACILITIES EACH YEAR",
+        "Adding value to Morocco's cityscape, one project at a time.",
     },
     {
       id: 3,
-      Number: "2.8%",
+      Number: "CONTRIBUTING TO TANGER'S ECONOMIC GROWTH",
       description:
-        "OF THE WORLD'S GDP FLOWS THROUGH OUR DISTRIBUTION CENTERS ANNUALLY",
+        "Our real estate developments play a significant role in the city's economic upliftment.",
     },
     {
       id: 4,
-      Number: "1.1 MILLION",
-      description: "PEOPLE WORK UNDER OUR ROOFS EVERYDAY",
+      Number: "500+ PEOPLE EMPLOYED ANNUALLY",
+      description: "Through our projects, we generate numerous jobs and foster local employment.",
     },
     {
       id: 5,
-      Number: "25,000",
+      Number: "IMPACTING 1,000+ LIVES THROUGH OUR COMMUNITY INITIATIVES",
       description:
-        "INDIVIDUALS TO BE TRAINED BY THROUGH OUR COMMUNITY WORKFORCE INITIATIVE",
+        "commitment extends beyond business, with a focus on enriching local communities.",
     },
   ];
 
@@ -44,30 +44,43 @@ const Global = () => {
     <div className="bg-video bg-overlay cursor-auto">
       <div className="mx-auto w-11/12 text-white py-12">
         <h2 className="text-4xl font-bold w-11/12 leading-large">
-          GLOBAL SCALE WITH LOCAL IMPACT
+        A NATIONAL REACH WITH LOCAL IMPACT
         </h2>
         <p className="md:leading-10 leading-7 md:text-xl text-white md:font-light md:tracking-wider md:text-base-400 md:w-8/12">
-          SEE HOW OUR PARTNERSHIPS HELP MAKE A REAL DIFFERENCE BOTH IN OUR LOCAL
-          COMMUNITIES AND AROUND THE WORLD.
+        Our partnerships have real, tangible effects in our local communities and around the nation.
         </p>
         <Swiper
           grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={window.innerWidth <= 768 ? 1 : 2}
+          centeredSlides={false}
+          slidesPerView={window.innerWidth <= 768 ? 1 : 3}
           navigation={true}
           modules={[Navigation]}
           className="mySwiper mb-10 "
         >
           {data.map((d) => (
             <SwiperSlide key={d.id}>
+              {
+              d.id === 1
+              ?
               <div className="ml-12 mr-10 md:mx-0 mt-12 md:mt-4 ">
-                <h3 className="font-bold text-3xl md:mb-3 md:w-6/12 md:text-center">
+                <h3 className="font-bold text-2xl md:mb-3  md:text-center">
                   {d.Number}
                 </h3>
-                <p className="text-base-400 text-xl md:text-sm md:w-6/12 md:text-center">
+                <p className="text-base-400 text-xl md:text-sm  md:text-center">
                   {d.description}
                 </p>
               </div>
+              :
+              <div className="ml-12 mr-10 md:mx-0 mt-12 md:mt-4 ">
+              <h3 className="font-bold text-2xl md:mb-3  md:text-center">
+                {d.Number}
+              </h3>
+              <p className="text-base-400 text-xl md:text-sm  md:text-center">
+                {d.description}
+              </p>
+            </div>
+              }
+              
             </SwiperSlide>
           ))}
         </Swiper>
