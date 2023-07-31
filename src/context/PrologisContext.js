b35eba37aadad79367869a4915159dec6b2b2411
ayoub129 <80260703+ayoub129@users.noreply.github.com>
2023-07-31 16:18:55 +0100

    Update PrologisContext.js

--- src/context/PrologisContext.js
+++ src/context/PrologisContext.js
@@ -1,14 +1,33 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer , useEffect , useState } from "react";
 import PrologisReducers from "./PrologisReducers";
-import { Propertiesdata } from "../components/Propertiesdata";
+import Supabase from "../components/Supabase";
 
 const PrologisContext = createContext();
 
 export const PrologisProvider = ({ children }) => {
+
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the 'properties' table in your Supabase project
+    const fetchData = async () => {
+      const { data: houses_cars, error } = await Supabase.from('houses-cars').select('*');
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else {
+        setProperties(houses_cars);
+      }
+    };
+
+    fetchData();
+    // eslint-disable-next-line
+  }, [Supabase]);
+
   const initialState = {
-    properties: Propertiesdata,
+    properties: properties,
     filterproperties: "All",
   };
+  
 
   const [state, dispatch] = useReducer(PrologisReducers, initialState);
 
